c5759481c6bd0385a6fcdb2c03f2cf0320a7f529
gruvector <vector.gru316@gmail.com>
2023-12-19 11:00:47 +0900

    property init commit

--- templates/favorite/view.php
+++ templates/favorite/view.php
@@ -4,27 +4,62 @@ if (! defined('ABSPATH')) {
 }
 ?>
 <style>
+    
 
+  
+  
+  
 </style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.min.js"></script>
 <div class="">
-    <!-- <?php
-        foreach ($this->favorite_list as $i => $record) {
-
-                echo '<label><input type="radio" name="account_attr_id" value="' . $record->ID . '" ' . $checked . ' >' . $record->nm . '</label>';
-            
-        }
-    ?>  -->
-
-    <div class="gm-card">
-        <div class="gm-card-img">
-            <img src={} alt="img" class="">
-        </div>
-        <div class="gm-card-info">
-            <p class="gm-card-info__p">{}</p>
-            <p class="gm-card-info__p">{}</p>
-        </div>
-        <div class="gm-card-button">
-            <input type="button" value="詳細を見る" class="gm-mypage-add-button">
-        </div>
+    <div class="testimonials-slider">
+        <?php
+            foreach ($this->favorite_list as $i => $record) { ?>
+            <div class="gm-card testimonial">
+                <div class="gm-card-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/library/images/<?php echo $record->imgs ?>.jpg" alt="img" >
+                </div>
+                <div class="gm-card-info">
+                    <div class="gm-card-info__div">車庫名: <?php echo $record->nm ?></div>
+                    <div class="gm-card-info__div">価格: <?php echo $record->fee_monthly_rent ?></div>
+                </div>
+                <div class="gm-card-button">
+                    <input type="button" value="詳細を見る" class="gm-mypage-add-button">
+                </div>
+            </div>
+                    
+        <?php        
+            }
+        ?>
     </div>
+
+<script>
+    $(document).ready(function(){
+        $('.testimonials-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: false,
+            arrows: false,
+            responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                slidesToShow: 1
+                }
+            }
+            ]
+        });
+    });
+</script>
+
+    
 </div>
